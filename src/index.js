658520d6da5b3592d7d2aa48004eb8534325214e
dylanmnr <reaky@outlook.fr>
2025-08-19 16:26:45 +0200

    mise en place des routes et ajout commentaires dans le fichier index

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express"; // Framework pour le serveur node
+import dotenv from "dotenv"; // Lit les variables d'environnement
+import cookieParser from "cookie-parser"; // Lit les contenu des cookies
+import cors from "cors";
+
+import routes from "./routes/index.js"; // Précise où sont les routes
+
+import { connectDB } from "./lib/db.js"; // Récupère la connexion à la base de données
+
+dotenv.config(); // Indique que l'ont va utiliser .env
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json()); // Indique que l'ont va pouvoir traduire le JSON et que l'ont va utiliser des cookies
+app.use(cookieParser());
+
+app.use("/", routes); // Chaque route localhost:3000 sera dirigé vers le dossier routes
+
+app.listen(PORT, () => {
+  console.log(`Le serveur est démarré sur le port ${PORT}`);
+  connectDB();
+});
---
+++ src/routes/user.route.js
@@ -0,0 +1,9 @@
+import express, { Router } from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json("TEST USER");
+});
+
+export default router;
